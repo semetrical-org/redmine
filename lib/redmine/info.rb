19eae00f7d63e1593b52909f23193323e7924eb5
mch735 <k735michael@gmail.com>
2020-10-28 11:12:28 +0300

    Fix css styles

--- lib/redmine/info.rb
+++ lib/redmine/info.rb
@@ -3,8 +3,8 @@
 module Redmine
   module Info
     class << self
-      def app_name; 'Redmine' end
-      def url; 'https://www.redmine.org/' end
+      def app_name; 'Semetrical Ltd' end
+      def url; 'https://www.semetrical.com/' end
       def help_url; 'https://www.redmine.org/guide' end
       def versioned_name; "#{app_name} #{Redmine::VERSION}" end
 
