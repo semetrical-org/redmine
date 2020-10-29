ce7f46c8db2a99119364d1d0babff5baafe9593c
mch735 <k735michael@gmail.com>
2020-10-29 17:31:07 +0300

    Moved application menu to top menu

--- lib/redmine.rb
+++ lib/redmine.rb
@@ -206,8 +206,46 @@ Redmine::MenuManager.map :top_menu do |menu|
             :if => Proc.new {User.current.logged?}
   menu.push :projects, {:controller => 'projects', :action => 'index'},
             :caption => :label_project_plural
+  menu.push :activity, {:controller => 'activities', :action => 'index'}
+  menu.push :issues,   {:controller => 'issues', :action => 'index'},
+            :if => Proc.new {
+              User.current.logged? &&
+                User.current.allowed_to?(:view_issues, nil, :global => true) &&
+                  EnabledModule.exists?(:project => Project.visible, :name => :issue_tracking)
+            },
+            :caption => :label_issue_plural
+  menu.push :time_entries, {:controller => 'timelog', :action => 'index'},
+            :if => Proc.new {
+              User.current.logged? &&
+                User.current.allowed_to?(:view_time_entries, nil, :global => true) &&
+                  EnabledModule.exists?(:project => Project.visible, :name => :time_tracking)
+            },
+            :caption => :label_spent_time
+  # menu.push :gantt, { :controller => 'gantts', :action => 'show' },
+  #           :if => Proc.new {
+  #             User.current.logged? &&
+  #               User.current.allowed_to?(:view_gantt, nil, :global => true) &&
+  #                 EnabledModule.exists?(:project => Project.visible, :name => :gantt)
+  #           },
+  #           :caption => :label_gantt
+  menu.push :calendar, { :controller => 'calendars', :action => 'show' },
+            :if => Proc.new {
+              User.current.logged? &&
+                User.current.allowed_to?(:view_calendar, nil, :global => true) &&
+                  EnabledModule.exists?(:project => Project.visible, :name => :calendar)
+            },
+            :caption => :label_calendar
+  menu.push :news, {:controller => 'news', :action => 'index'},
+            :if => Proc.new {
+              User.current.logged? &&
+                User.current.allowed_to?(:view_news, nil, :global => true) &&
+                  EnabledModule.exists?(:project => Project.visible, :name => :news)
+            },
+            :caption => :label_news_plural
   menu.push :administration, {:controller => 'admin', :action => 'index'},
-            :if => Proc.new {User.current.admin?}, :last => true
+            :if => Proc.new {
+              User.current.logged? && User.current.admin?
+            }, :last => true
   menu.push :help, Redmine::Info.help_url, :last => true
 end
 
@@ -219,43 +257,6 @@ Redmine::MenuManager.map :account_menu do |menu|
             :if => Proc.new {User.current.logged?}
   menu.push :logout, :signout_path, :html => {:method => 'post'},
             :if => Proc.new {User.current.logged?}
-end
-
-Redmine::MenuManager.map :application_menu do |menu|
-  menu.push :projects, {:controller => 'projects', :action => 'index'},
-            :permission => nil,
-            :caption => :label_project_plural
-  menu.push :activity, {:controller => 'activities', :action => 'index'}
-  menu.push :issues,   {:controller => 'issues', :action => 'index'},
-            :if => Proc.new {
-                     User.current.allowed_to?(:view_issues, nil, :global => true) &&
-                       EnabledModule.exists?(:project => Project.visible, :name => :issue_tracking)
-                   },
-            :caption => :label_issue_plural
-  menu.push :time_entries, {:controller => 'timelog', :action => 'index'},
-            :if => Proc.new {
-                     User.current.allowed_to?(:view_time_entries, nil, :global => true) &&
-                       EnabledModule.exists?(:project => Project.visible, :name => :time_tracking)
-                   },
-            :caption => :label_spent_time
-  menu.push :gantt, { :controller => 'gantts', :action => 'show' },
-            :caption => :label_gantt,
-            :if => Proc.new {
-                     User.current.allowed_to?(:view_gantt, nil, :global => true) &&
-                       EnabledModule.exists?(:project => Project.visible, :name => :gantt)
-                   }
-  menu.push :calendar, { :controller => 'calendars', :action => 'show' },
-            :caption => :label_calendar,
-            :if => Proc.new {
-                     User.current.allowed_to?(:view_calendar, nil, :global => true) &&
-                       EnabledModule.exists?(:project => Project.visible, :name => :calendar)
-                   }
-  menu.push :news, {:controller => 'news', :action => 'index'},
-            :if => Proc.new {
-                     User.current.allowed_to?(:view_news, nil, :global => true) &&
-                       EnabledModule.exists?(:project => Project.visible, :name => :news)
-                   },
-            :caption => :label_news_plural
 end
 
 Redmine::MenuManager.map :admin_menu do |menu|
