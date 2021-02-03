6fe780907e54bc5dfaed228723fb2a230f5e21f6
mch735 <k735michael@gmail.com>
2021-02-03 12:36:18 +0300

    Changed deploy settings

--- Capfile
+++ Capfile
@@ -6,6 +6,7 @@ require 'capistrano/deploy'
 require 'capistrano/scm/git'
 install_plugin Capistrano::SCM::Git
 
+require 'capistrano/nvm'
 require 'capistrano/rvm'
 require 'capistrano/bundler'
 require 'capistrano/rails/migrations'
--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,10 @@ set :deploy_to, "/var/www/#{fetch(:application)}"
 
 set :pty, false
 
+set :nvm_type, :system
+set :nvm_node, 'v10.19.0'
+set :nvm_map_bins, %w[node npm yarn webpack rake]
+
 set :rvm_ruby_version, 'ruby-2.5.7'
 set :rvm_type, :system
 
@@ -24,6 +28,6 @@ set :foreman_systemd_user, 'deploy'
 set :maintenance_timeout, 10
 
 before 'deploy', 'maintenance:enable'
-after 'deploy', 'foreman_systemd:stop'
+# after 'deploy', 'foreman_systemd:stop'
 after 'deploy', 'foreman_systemd:setup'
 after 'deploy', 'maintenance:disable'
